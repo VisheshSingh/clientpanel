0c809708b769130d2956dff050f42c437f6b3c4d
Vishesh <visheshsingh16@gmail.com>
2018-06-08 20:09:27 -0500

    Getting the client from firestore

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ClientService } from "../../services/client.service";
+import { Client } from "../../models/Client";
+import { Router, ActivatedRoute } from "@angular/router";
+import { FlashMessagesService } from "angular2-flash-messages";
 
 @Component({
-  selector: 'app-client-details',
-  templateUrl: './client-details.component.html',
-  styleUrls: ['./client-details.component.css']
+  selector: "app-client-details",
+  templateUrl: "./client-details.component.html",
+  styleUrls: ["./client-details.component.css"]
 })
 export class ClientDetailsComponent implements OnInit {
+  id: string;
+  client: Client;
+  hasBalance: boolean = false;
+  showBalanceUpdateInput: boolean = false;
 
-  constructor() { }
+  constructor(
+    private cs: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessage: FlashMessagesService
+  ) {}
 
   ngOnInit() {
+    // Get id from url
+    this.id = this.route.snapshot.params["id"];
+    // Get Client
+    this.cs.getClient(this.id).subscribe(client => {
+      this.client = client;
+      console.log(this.client);
+    });
   }
-
 }
