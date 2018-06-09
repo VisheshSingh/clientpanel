d1d6b702d56ca1a775781c4d8633019cf8ddeaea
Vishesh <visheshsingh16@gmail.com>
2018-06-08 19:29:14 -0500

    Add Client Form and validation using template driven forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { environment } from "../environments/environment";
 import { AngularFireModule } from "angularfire2";
 import { AngularFirestoreModule } from "angularfire2/firestore";
 import { AngularFireAuthModule } from "angularfire2/auth";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
@@ -40,7 +41,8 @@ import { ClientService } from "./services/client.service";
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase, "clientpanel"),
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    FormsModule
   ],
   providers: [ClientService],
   bootstrap: [AppComponent]
