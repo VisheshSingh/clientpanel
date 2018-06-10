91fc2d67b369cd2c307179c0f6b8ecb4a24103d9
Vishesh <visheshsingh16@gmail.com>
2018-06-10 11:50:41 -0500

    User registration

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { FlashMessagesService } from "angular2-flash-messages";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private fms: FlashMessagesService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit() {
+    this.authService
+      .register(this.email, this.password)
+      .then(res => {
+        this.fms.show("You are now registered and logged in!", {
+          cssClass: "alert-success",
+          timeout: 4000
+        });
+        this.router.navigate(["/"]);
+      })
+      .catch(err => {
+        this.fms.show(err.message, {
+          cssClass: "alert-danger",
+          timeout: 4000
+        });
+      });
   }
-
 }
