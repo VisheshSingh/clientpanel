a822640ada6170425b1cd0724edf66b93d64c461
Vishesh <visheshsingh16@gmail.com>
2018-06-10 20:27:09 -0500

    Adding the settings page

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { FlashMessagesService } from "angular2-flash-messages";
+import { SettingsService } from "../../services/settings.service";
+import { Settings } from "../../models/Settings";
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  selector: "app-settings",
+  templateUrl: "./settings.component.html",
+  styleUrls: ["./settings.component.css"]
 })
 export class SettingsComponent implements OnInit {
+  settings: Settings;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private flashMessage: FlashMessagesService,
+    private ss: SettingsService
+  ) {}
 
   ngOnInit() {
+    this.settings = this.ss.getSettings();
   }
 
+  onSubmit() {
+    this.ss.changeSettings(this.settings);
+    this.flashMessage.show("Seetings saved!", {
+      cssClass: "alert-success",
+      timeout: 4000
+    });
+  }
 }
